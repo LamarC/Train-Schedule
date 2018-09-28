480a93f0c94ef59d5853aaddbf34bab22270b746
LamarC <dviann89@gmail.com>
2018-09-28 13:41:22 -0500

    Doc base created

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCRlvsiktEcC_HEz0F5h1HVX5dF9gaDzHc",
+    authDomain: "train-schedule-61b7c.firebaseapp.com",
+    databaseURL: "https://train-schedule-61b7c.firebaseio.com",
+    projectId: "train-schedule-61b7c",
+    storageBucket: "train-schedule-61b7c.appspot.com",
+    messagingSenderId: "725778773260"
+  };
+  firebase.initializeApp(config);
+
+  
