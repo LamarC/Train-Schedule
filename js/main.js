dd6ed66c8f1c0ce932163b4ac71f54ecec6dacc0
LamarC <dviann89@gmail.com>
2018-09-28 19:00:27 -0500

    Var created

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-// Initialize Firebase
-var config = {
+ // Initialize Firebase
+ var config = {
     apiKey: "AIzaSyCRlvsiktEcC_HEz0F5h1HVX5dF9gaDzHc",
     authDomain: "train-schedule-61b7c.firebaseapp.com",
     databaseURL: "https://train-schedule-61b7c.firebaseio.com",
@@ -7,6 +7,40 @@ var config = {
     storageBucket: "train-schedule-61b7c.appspot.com",
     messagingSenderId: "725778773260"
   };
+
   firebase.initializeApp(config);
 
-  
+
+
+var database = firebase.database();
+
+$(".submit").on("click", function (event) {
+    event.preventDefault();
+
+    var name = $("<#name>").val().trim();
+    var dest = $("<#destination>").val().trim();
+    var next = $("<#train-time>").val().trim();
+    var freq = $("<frequency>").val().trim();
+
+
+    var train = {
+        name : name,
+        dest : dest,
+        next : next,
+        freq : freq,
+};
+
+console.log(train);
+
+database.ref().push(train);
+
+}); 
+
+
+
+
+
+
+/*$(document).ready(function () {
+    loadDocument();
+};*/ 
